9e7eb1c1990b056b9d2b520ce38c94281143c697
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-04-13 22:31:06 -0700

    saving work

--- hw2/mult.sv
+++ hw2/mult.sv
@@ -4,60 +4,69 @@ module multiply #(parameter WIDTH = 8)
     input[WIDTH-1:0] multiplier, // Q
     output[(WIDTH*2)-1:0] product // P
 );
+
+    parameter productBitWidth = (WIDTH*2)-1;
+    parameter lastCoutIndex = WIDTH-2;
+    parameter numRCA = WIDTH-1;
+    parameter lastRcaIndex = WIDTH-2;
+    wire[WIDTH-1:0][WIDTH-1:0] partialProducts;
+    wire[WIDTH-1:0][lastRcaIndex:0] rcaInputA;
+    wire[WIDTH-1:0][lastRcaIndex:0] rcaInputB;
+    wire[WIDTH-1:0][lastRcaIndex:0] rcaOutput;
+    wire[lastCoutIndex:0] cOut;
     
-    // set of wires for the zeroeth partial product because its special.
-    // you dont need WIDTH-1 because the zeroeth partial product is assigned to the zeroeth output
-    parameter firstThroughN = WIDTH-2;
-    parameter firstThroughNminusOne = WIDTH-3;
-    // Index for last RCA
-    parameter lastRcaEndIndex = (WIDTH*2)-2;
-    parameter lastRcaBeginIndex = ((WIDTH*2)-1)-WIDTH;
-    parameter lastRcaCarryIndex = (WIDTH*2)-1;
-    parameter lastPartialProduct = WIDTH-3;
-    parameter lastPartialSum = WIDTH-2;
-    parameter lastCarry = WIDTH-2;
-    parameter numRcas = WIDTH-1;
+    genvar i;
+    //integer iter;
+    generate
+        for(i = 0; i < WIDTH; i++)
+        begin: gen_partial_products
+            assign #1 partialProducts[i] = multiplicand&{WIDTH{multiplier[i]}};
+        end
+    endgenerate
 
-    wire[firstThroughN:0] initialPartialProduct;
+    generate
+        for(i = 0; i < numRCA; i++)
+        begin
+            rca #(.WIDTH(WIDTH)) adder(rcaOutput[i], cOut[i], rcaInputA[i], rcaInputB[i], 1'b0);
+        end
+    endgenerate
 
-    // WIDTH-1 packed AND gates X WIDTH-2 number of iters
-    wire[WIDTH-1:0][firstThroughN:0] partialProduct;
-    wire[firstThroughNminusOne:0] carryOut;
-    // first sum result becomes a product, unless its the last set of RCAs
-    wire[firstThroughN:0][firstThroughNminusOne:0] partialSum;
+    // assign to RCA 0 through WIDTH-1, input A partial products
+    generate
+        for(i = 0; i < numRCA; i++)
+        begin
+            assign rcaInputA[i] = partialProducts[i+1];
+        end
+    endgenerate
 
-    // initial set of ANDS
-    assign #1 product[0] = (multiplicand[0]&multiplier[0]);
-    
-    genvar i, j;
+    // extract the zeroeth partial product
+    wire[WIDTH-1:0] zeroethPp = partialProducts[0];
+    // zeroeth RCA input B gets a 0 on the MSB and upper width-1 bits of the partial product
+    assign rcaInputB[0] = {1'b0, zeroethPp[WIDTH-1:1]};
+
+    // first through numRCA RCA input B gets cout on the MSB the the upper width-1 bits of the previous sum
+    generate
+        for(i = 1; i < numRCA; i++)
+        begin
+            wire[WIDTH-1:0] prevRcaSum = rcaOutput[i-1];
+            assign rcaInputB[i] = {cOut[i-1], prevRcaSum[WIDTH-1:1]};
+        end
+    endgenerate
+
+    // assign product outputs
+    assign product[0] = zeroethPp[0];
     generate
         for(i = 1; i < (WIDTH-1); i++)
-        begin: gen_partial_products
-            assign initialPartialProduct[i] = multiplicand[i]&multiplier[0];
-        end
-    endgenerate
-    
-    // generate WIDTH-1 number of ANDS, WIDTH-2 times
-    generate
-        for(i = 0; i < (firstThroughN); i++) // 1 - Full width (2 - WIDTH when thinking zero indexed)
         begin
-            for(j = 0; j < (WIDTH - 1); j++) // full width (1 - WIDTH when thinking zero indexed)
-            begin
-                assign partialProduct[j][i] = multiplicand[j]&multiplier[i];
-            end
+            //extract the current iter's RCA output
+            wire[WIDTH-1:0] tempRca = rcaOutput[i];
+            // extract the first RCA output, assign the prodcut to it
+            assign product[i] = tempRca[0];
         end
     endgenerate
-    
-    // first adder
-    rca #(.WIDTH(WIDTH)) firstAdder({partialSum[0], product[1]}, carryOut[0], partialProduct[0], {1'b0, initialPartialProduct}, 1'b0);
-    // last adder
-    rca #(.WIDTH(WIDTH)) lastAdder(product[lastRcaEndIndex:lastRcaBeginIndex], product[lastRcaCarryIndex], partialProduct[lastPartialProduct], {carryOut[lastCarry], partialSum[lastPartialSum]}, 1'b0);
-    generate
-        for(i = 0; i < numRcas; i++)
-        begin: gen_adders
-            // Middle adders
-            rca #(.WIDTH(WIDTH)) adder({partialSum[i], product[i+1]}, carryOut[i], partialProduct[i], {carryOut[i-1], partialSum[i-1]}, 1'b0);
-        end
-    endgenerate
+    // extract the last RCA output
+    wire[WIDTH-1:0] lastRca = rcaOutput[lastRcaIndex];
+    //assign product[productBitWidth:WIDTH-1] = {cOut[lastCoutIndex], lastRca};
+    assign product[productBitWidth:WIDTH-1] = 0;
     
 endmodule
