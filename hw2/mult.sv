4482d572ae4b8e037e1718bc7f22a636acae66c9
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-04-14 19:38:00 -0700

    debugging code

--- hw2/mult.sv
+++ hw2/mult.sv
@@ -2,17 +2,24 @@ module multiply #(parameter WIDTH = 8)
 (
     input[WIDTH-1:0] multiplicand, // M
     input[WIDTH-1:0] multiplier, // Q
-    output[(WIDTH*2)-1:0] product // P
+    output[(WIDTH*2)-1:0] product, // P
+    output[(WIDTH)-1:0] rcaInputB_D, // debugging
+    output carryOut, // debugging
+    output[(WIDTH)-1:0] rcaInputA_D,
+    output[WIDTH-1:0] partialProducts_D
 );
 
+    //wire[packed][unpacked] myWire
+    //wire[unpacked][packed] myWire????
+    //wire[packed] = mywire[unpacked][packed]
     parameter productBitWidth = (WIDTH*2)-1;
     parameter lastCoutIndex = WIDTH-2;
     parameter numRCA = WIDTH-1;
     parameter lastRcaIndex = WIDTH-2;
     wire[WIDTH-1:0][WIDTH-1:0] partialProducts;
-    wire[WIDTH-1:0][lastRcaIndex:0] rcaInputA;
-    wire[WIDTH-1:0][lastRcaIndex:0] rcaInputB;
-    wire[WIDTH-1:0][lastRcaIndex:0] rcaOutput;
+    wire[lastRcaIndex:0][WIDTH-1:0] rcaInputA;
+    wire[lastRcaIndex:0][WIDTH-1:0] rcaInputB;
+    wire[lastRcaIndex:0][WIDTH-1:0] rcaOutput;
     wire[lastCoutIndex:0] cOut;
     
     genvar i;
@@ -20,7 +27,7 @@ module multiply #(parameter WIDTH = 8)
     generate
         for(i = 0; i < WIDTH; i++)
         begin: gen_partial_products
-            assign #1 partialProducts[i] = multiplicand&{WIDTH{multiplier[i]}};
+            assign #1 partialProducts[i] = multiplicand&({WIDTH{multiplier[i]}});
         end
     endgenerate
 
@@ -39,34 +46,41 @@ module multiply #(parameter WIDTH = 8)
         end
     endgenerate
 
-    // extract the zeroeth partial product
-    wire[WIDTH-1:0] zeroethPp = partialProducts[0];
     // zeroeth RCA input B gets a 0 on the MSB and upper width-1 bits of the partial product
-    assign rcaInputB[0] = {1'b0, zeroethPp[WIDTH-1:1]};
+    assign rcaInputB[0] = {1'b0, partialProducts[0][WIDTH-1:1]};
 
     // first through numRCA RCA input B gets cout on the MSB the the upper width-1 bits of the previous sum
     generate
         for(i = 1; i < numRCA; i++)
         begin
-            wire[WIDTH-1:0] prevRcaSum = rcaOutput[i-1];
-            assign rcaInputB[i] = {cOut[i-1], prevRcaSum[WIDTH-1:1]};
+            assign rcaInputB[i] = {cOut[i-1], rcaOutput[i-1][WIDTH-1:1]};
         end
     endgenerate
 
     // assign product outputs
-    assign product[0] = zeroethPp[0];
+    assign product[0] = partialProducts[0][0];
     generate
         for(i = 1; i < (WIDTH-1); i++)
         begin
-            //extract the current iter's RCA output
-            wire[WIDTH-1:0] tempRca = rcaOutput[i];
             // extract the first RCA output, assign the prodcut to it
-            assign product[i] = tempRca[0];
+            assign product[i] = rcaOutput[i-1][0];
         end
     endgenerate
+
+    generate
+        for(i = 0; i < WIDTH; i++)
+        begin
+            assign product[WIDTH-1+i] = rcaOutput[lastRcaIndex][i];
+        end
+    endgenerate
+    assign product[productBitWidth] = cOut[lastCoutIndex];
+    assign rcaInputB_D = rcaInputB[0][WIDTH-1:0];
+    assign carryOut = cOut[0];
+    assign rcaInputA_D = rcaInputA[1][WIDTH-1:0];
+    assign partialProducts_D = partialProducts[2][WIDTH-1:0];
     // extract the last RCA output
-    wire[WIDTH-1:0] lastRca = rcaOutput[lastRcaIndex];
-    //assign product[productBitWidth:WIDTH-1] = {cOut[lastCoutIndex], lastRca};
-    assign product[productBitWidth:WIDTH-1] = 0;
-    
+    //assign product[productBitWidth:productBitWidth-WIDTH] = {cOut[lastCoutIndex], rcaOutput[lastRcaIndex][WIDTH-1:0]};
+    //7th bit, zero indexed
+    //assign product[productBitWidth:productBitWidth-WIDTH] = {cOut[lastCoutIndex],0};
+
 endmodule
--- hw2/multtb.sv
+++ hw2/multtb.sv
@@ -7,6 +7,10 @@ module top #(parameter WIDTH = 8);
 	reg [WIDTH-1:0] multiplicand, multiplier;
     // Outputs from DUT
     wire[(WIDTH*2)-1:0] product;
+    wire[(WIDTH)-1:0] rcaInputB;
+    wire carryOut;
+    wire[(WIDTH)-1:0] rcaInputA;
+    wire[WIDTH-1:0] partialProducts_D;
 
     localparam NUM_ITERS = 2**((WIDTH*2));
     reg[NUM_ITERS -1:0] iter;
@@ -15,7 +19,7 @@ module top #(parameter WIDTH = 8);
 
     multiply #(.WIDTH(WIDTH)) dut
     (
-        .multiplicand(multiplicand), .multiplier(multiplier), .product(product)
+        .multiplicand(multiplicand), .multiplier(multiplier), .product(product), .rcaInputB_D(rcaInputB), .carryOut(carryOut), .rcaInputA_D(rcaInputA), .partialProducts_D(partialProducts_D)
     );
 
     initial
@@ -28,12 +32,12 @@ module top #(parameter WIDTH = 8);
             if(expectedResult === product)
             begin
                 `ifdef DEBUG
-                    $display("Iteration %0b, PASS. expected product: %0b,  multiplicand, M = %0b, multiplier, Q = %0b, product, P %0b \n", iter, expectedResult, multiplicand, multiplier, product);
+                    $display("Iteration %0b, PASS. expected product: %0b,  multiplicand, M = %0b, multiplier, Q = %0b, product, P %0b, rcaInputB = %b, carryOut = %0b, rcaInputA = %b, partialProducts_D = %b\n", iter, expectedResult, multiplicand, multiplier, product, rcaInputB, carryOut, rcaInputA, partialProducts_D);
                 `endif
             end
             else
             begin
-                $display("Iteration %0b, FAIL. expected product: %0b,  multiplicand, M = %0b, multiplier, Q = %0b, product, P %0b \n", iter, expectedResult, multiplicand, multiplier, product);
+                $display("Iteration %0b, FAIL. expected product: %0b,  multiplicand, M = %0b, multiplier, Q = %0b, product, P %0b, rcaInputB = %b, carryOut = %0b, rcaInputA = %b, partialProducts_D = %b\n", iter, expectedResult, multiplicand, multiplier, product, rcaInputB, carryOut, rcaInputA, partialProducts_D);
                 fails++;
             end
         end
