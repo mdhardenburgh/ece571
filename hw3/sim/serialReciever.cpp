1e1a1094b18e05837697ee4ad7f45c351d68f57c
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-04-18 21:33:12 -0700

    saving work

--- hw3/sim/serialReciever.cpp
+++ hw3/sim/serialReciever.cpp
@@ -7,8 +7,7 @@
 
 SerialReciver::SerialReciver()
 {
-    m_outputFile.open(m_outputFileName, std::ofstream::out | std::ofstream::app);
-    m_inputFile.open(m_inputFileName, std::ifstream::in);
+
 }
 
 SerialReciver::~SerialReciver()
@@ -23,106 +22,196 @@ SerialReciver::~SerialReciver()
     }
 }
 
+void SerialReciver::setOutputFile(std::string fileName)
+{
+    m_outputFileName = fileName;
+}
+
+void SerialReciver::setInputFile(std::string fileName)
+{
+    m_inputFileName = fileName;
+}
+
 void SerialReciver::mainLoop()
 {
+    m_outputFile.open(m_outputFileName, std::ofstream::out | std::ofstream::trunc);
+    m_inputFile.open(m_inputFileName, std::ifstream::in);
+    
     // if no error
     while(!nextState())
     {
         m_clockCycle++;
     }
+    m_outputFile << m_clockCycle << " clock cycles"; 
 }
 
 bool SerialReciver::nextState()
 {
-    bool error = false; 
     uint32_t in = 0x0;
     bool reset = false;
-    
-    switch(m_currentState)
+    bool error = readInput(in, reset);
+    if(!error)
     {
-        case State::RESET:
-        {   
-            m_bitCounter = 0;
-            m_outByte = 0x0;
-            error = readInput(in, reset);
+        writeOutput();
 
-            if(reset == true)
-            {
-                m_currentState = State::RESET;
-                break;
-            }
-            else
-            {
-                m_currentState = State::IDLE;
-                break;
-            }
-        }
-        case State::IDLE:
+        switch(m_currentState)
         {
-            m_bitCounter = 0;
-            m_outByte = 0x0;
-            error = readInput(in, reset);
+            case State::RESET:
+            {   
+                m_bitCounter = 0;
+                m_outByte = 0x0;
+                m_doneBit = 0x0;
 
-            if(reset == true)
-            {
-                m_currentState = State::RESET;
-                break;
-            }
-            else
-            {
-                if(in == 0x0)
+                if(reset == true)
                 {
-                    m_currentState = State::RECIEVE;
+                    m_currentState = State::RESET;
                     break;
                 }
-                else if(in == 0x1)
+                else if(reset == false)
                 {
                     m_currentState = State::IDLE;
                     break;
                 }
+                else
+                {
+                    m_currentState = State::EXCEPTION;
+                    break;
+                }
             }
-        }
-        case State::RECIEVE:
-        {
-            error = readInput(in, reset);
+            case State::IDLE:
+            {
+                if(reset == true)
+                {
+                    m_currentState = State::RESET;
+                    break;
+                }
+                else if(reset == false)
+                {
+                    if(in == 0x0)
+                    {
+                        m_currentState = State::RECIEVE;
+                        break;
+                    }
+                    else if(in == 0x1)
+                    {
+                        m_currentState = State::IDLE;
+                        break;
+                    }
+                    else
+                    {
+                        m_currentState = State::EXCEPTION;
+                        break;
+                    }
+                }
+                else
+                {
+                    m_currentState = State::EXCEPTION;
+                    break;
+                }
+            }
+            case State::RECIEVE:
+            {
+                if(reset == true)
+                {
+                    m_currentState = State::RESET;
+                    break;
+                }
+                else if(reset == false)
+                {
+                    if(m_bitCounter<8)
+                    {
+                        m_outByte = m_outByte<<1;
+                        m_outByte |= in;
+                        m_bitCounter++;
+                        m_currentState = State::RECIEVE;
+                        break;
+                    }
+                    else
+                    {
+                        // have recived stop bit
+                        if((in == 1) && (m_bitCounter == 8))
+                        {
+                            m_bitCounter = 0;
+                            m_doneBit = 1;
+                            m_currentState = State::END;
+                            break;
+                        }
+                        else if(in == 0) // have not recieved stop bit
+                        {
+                            m_currentState = State::RECIEVE;
+                            m_bitCounter++;
+                            break;
+                        }
+                        else if((in == 1) && (m_bitCounter > 8))
+                        {
+                            m_outByte = 0;
+                            m_bitCounter = 0;
+                            m_currentState = State::IDLE;
+                            break;
+                        }
+                        else
+                        {
+                            m_currentState = State::EXCEPTION;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    m_currentState = State::EXCEPTION;
+                    break;
+                }
+            }
+            case State::END:
+            {
+                if(reset == true)
+                {
+                    m_currentState = State::RESET;
+                    break;
+                }
+                else if(reset == false)
+                {
+                    //m_outByte = 0;
+                    m_doneBit = 0;
+                    if(in == 0)
+                    {
+                        m_currentState = State::RECIEVE;
+                        break;
+                    }
+                    else if (in == 1)
+                    {
+                        m_currentState = State::IDLE;
+                        break;
+                    }
+                    else
+                    {
+                        m_currentState = State::EXCEPTION;
+                        break;
+                    }
+                }
+                else
+                {
+                    m_currentState = State::EXCEPTION;
+                    break;
+                }
+            } 
+            case State::EXCEPTION:
+            {
+                // we should never reach this state
+                // if we did, something truely went very wrong
+                std::cerr << "exception caught"<<std::endl;
 
-            if(reset == true)
-            {
-                m_currentState = State::RESET;
-                break;
-            }
-            else
-            {
-                if(m_bitCounter<7)
+                if(reset == true)
                 {
-                    m_outByte = m_outByte<<1;
-                    m_outByte |= in;
-                    m_bitCounter++;
-                    m_currentState = State::RECIEVE;
+                    m_currentState = State::RESET;
                     break;
                 }
-                else if(m_bitCounter == 7)
+                else
                 {
-                    m_outByte = m_outByte<<1;
-                    m_outByte |= in;
-                    m_bitCounter++;
-                    m_currentState = State::END;
+                    m_currentState =  State::EXCEPTION;
                     break;
                 }
             }
-        }
-        case State::END:
-        {
-            writeOutput();
-            m_outByte = 0;
-            writeDone();
-            m_currentState = State::IDLE;
-        } 
-        default:
-        {
-            // we should never reach this state
-            // if we did, something went very wrong
-            error = true;
         }
     }
     return error; 
@@ -143,6 +232,13 @@ bool SerialReciver::readInput(uint32_t& in, bool& reset)
         }
         else
         {
+            //strip anything from "//" to end‑of‑line
+            auto pos = line.find("//");
+            if (pos != std::string::npos)
+            {
+                line.erase(pos);
+            }
+
             if(std::regex_search(line, match, pattern))
             {
                 std::string str1(match[1]);
@@ -165,10 +261,5 @@ bool SerialReciver::readInput(uint32_t& in, bool& reset)
 
 void SerialReciver::writeOutput()
 {
-    m_outputFile << m_outByte << ", ";
-}
-
-void SerialReciver::writeDone()
-{
-    m_outputFile << 1 << std::endl;
+    m_outputFile << (uint32_t)m_outByte << ", "<< m_doneBit<<std::endl;
 }
