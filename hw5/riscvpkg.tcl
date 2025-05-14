886cdce68e46a126855abc066c9b66bed7e5fad5
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-05-13 23:28:00 -0700

    reverted tcl file

--- hw5/riscvpkg.tcl
+++ hw5/riscvpkg.tcl
@@ -5,9 +5,10 @@
 # use xvlog, xelab and xsim for simulation
 # read_verilog, synth_design for FPGA implemention
 
-exec xvlog -sv riscvpkg.sv top.sv
+exec xvlog -sv riscvpkg.sv top.sv -d DEBUG=1
 # lint files
-exec xelab top -generic_top "PARSE_ASSEMBLY=1" -generic_top "FILENAME=\"test\""
+# exec xelab top -generic_top "PARSE_ASSEMBLY=1" -generic_top "FILENAME=\"test\""
+exec xelab top -generic_top "PARSE_ASSEMBLY=0"
 
 exec xsim top -runall 
 
