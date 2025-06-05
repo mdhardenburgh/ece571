039c0d0cbf2ee0c682967b66bf0bd07ffdd1ec19
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-06-04 21:20:10 -0700

    saving work

--- hw8/top.sv
+++ hw8/top.sv
@@ -29,7 +29,7 @@ module top #(parameter AGENTS = 8);
         clock = 1'b0;
         forever #20 clock <= ~clock;
     end
-
+/*
     `ifdef request_vector_has_invalid_bits
     `TEST_TASK(arbiterAsserts, request_vector_has_invalid_bits)
         reset <= 1'b1;
@@ -74,46 +74,60 @@ module top #(parameter AGENTS = 8);
         @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b00000001);
     `END_TEST_TASK(arbiterAsserts, grant_after_reset)
-
+*/
     `TEST_TASK(arbiterAsserts, check_if_grant_is_produced_in_one_cycle)
         reset = 1'b1;
         r = 'b1001_1010;
         @(posedge clock);
+        // reset is only 1 after the above clock edge
+        @(posedge clock);
+        // reset is still 1 after the above clock edge
         EXPECT_EQ_LOGIC(g, 'b0);
+        @(posedge clock);
+        //reset is still 1 after the above clock edge
         reset = 1'b0;
         r = 'b1001_1010;
         @(posedge clock);
-        EXPECT_EQ_LOGIC(g, 'b0000_0010);
-        r = 'b1001_1011;
+        // reset is 0 after the above clock edge, r = 'b1001_1010, grant is 'b0000_0010, g is still 0
         @(posedge clock);
+        // NOW g is 'b0000_0010
+        EXPECT_EQ_LOGIC(g, 'b0000_0010);
+        // r = 'b1001_1011, grant is 'b0000_0011
+        r = 'b1001_1011;
+        repeat (2) @(posedge clock);
+        // NOW g is 'b0000_0001
         EXPECT_EQ_LOGIC(g, 'b0000_0001);
     `END_TEST_TASK(arbiterAsserts, check_if_grant_is_produced_in_one_cycle)
-
+/*
     `TEST_TASK(arbiterAsserts, verify_never_more_than_one_grant)
         reset = 1'b1;
         r = 'b1001_1010;
         @(posedge clock);
+        @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b0);
         reset = 1'b0;
         r = 'b1001_1010;
         @(posedge clock);
+        @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b0000_0010);
         r = 'b1111_1111;
         @(posedge clock);
+        @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b0000_0001);
         r = 'b0111_1110;
-        /*
+        @(posedge clock);
         @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b0000_0010);
+        /*
         r = 'b1000_0000;
         @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b1000_0000);
         r = 'b1000_1000;
         @(posedge clock);
         EXPECT_EQ_LOGIC(g, 'b0000_1000);
-        */
+
     `END_TEST_TASK(arbiterAsserts, verify_never_more_than_one_grant)
-/*
+
     `TEST_TASK(arbiterAsserts, single_requestor_always_recieves_grant)
         reset = 1'b1;
         r = 'b0000_0010;
