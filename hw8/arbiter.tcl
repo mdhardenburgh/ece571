80991fe00923c8ea0752a1e50ac8df59b5b086e3
Matthew Hardenburgh <matthew@hardenburgh.io>
2025-06-05 22:56:37 -0700

    deactivated intentional bug

--- hw8/arbiter.tcl
+++ hw8/arbiter.tcl
@@ -4,8 +4,9 @@
 # vivado -mode batch -source arbiter.tcl
 # use xvlog, xelab and xsim for simulation
 # read_verilog, synth_design for FPGA implemention
+# exec xvlog -sv arbiter.sv top.sv assertions.sv --define cause_after_grant_agent_does_not_contine_to_request_after_256_cycles
 
-exec xvlog -sv arbiter.sv top.sv assertions.sv --define cause_after_grant_agent_does_not_contine_to_request_after_256_cycles
+exec xvlog -sv arbiter.sv top.sv assertions.sv
 # lint files
 exec xelab top
 
